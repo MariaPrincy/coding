b7a9ce83f71549f5638f57497d0b179507915f57
MariaPrincy <53246875+MariaPrincy@users.noreply.github.com>
2019-07-24 14:13:58 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char a;
+if(isalpha(a))
+{
+if(a==A|| a==a || a==e || a==E || a==i || a==I || a==o || a==O || a==u || a==U )
+printf("vowel");
+else
+printf("consonant");
+}
+else
+printf("invalid")
+return(0);
+}
+
+
