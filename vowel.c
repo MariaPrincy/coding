df27456a76dc7c14cd2d2adbe0b7c116366a6079
MariaPrincy <53246875+MariaPrincy@users.noreply.github.com>
2019-07-24 14:26:41 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -3,10 +3,10 @@
 int main()
 {
 char a;
-  scanf("%d",&a);
+  scanf("%c",&a);
 if(isalpha(a))
 {
-if(a==A|| a=='a' || a=='e' || a=='E' || a=='i' || a=='I' || a=='o' || a=='O' || a=='u' || a=='U' )
+if(a=='A'|| a=='a' || a=='e' || a=='E' || a=='i' || a=='I' || a=='o' || a=='O' || a=='u' || a=='U' )
 printf("vowel");
 else
 printf("consonant");
